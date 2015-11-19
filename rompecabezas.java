e64b4ebe96722bf8b2f5ff171e80df2147dc62e5
Nacho Crespo <ignacio.crespo.martinez@gmail.com>
2015-11-19 19:13:47 +0100

    refactorizacion de las funciones del tablero

--- rompecabezas.java
+++ rompecabezas.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 class celda {
 	private boolean objetivo = false;
 	private Integer bola = null;
@@ -24,29 +25,18 @@ class celda {
 	}
 }
 
-public class rompecabezas {
-	private static int n = 0;
-	private static int m = 0;
-	private static int numeroBolas = 0;
-	private static celda[][] tablero;
-	public static void main(String[] arg) {
+class Tablero {
+	private celda[][] tablero;
+	private int n = 0;
+	private int m = 0;
 
-		Scanner scan = new Scanner(System.in);
-
-		n = scan.nextInt();
-		m = scan.nextInt();
-
-		inicializarTablero(n,m);
-		
-		numeroBolas = scan.nextInt();
-		leerCoordenadasPosiciones(scan,true);
-		leerCoordenadasPosiciones(scan,false);
-		imprimirTablero();
-
-		
+	public Tablero(int n, int m) {
+		this.n = n;
+		this.m = m;
+		inicializar();
 	}
 
-	private static void inicializarTablero(int n, int m) {
+	private void inicializar() {
 		tablero = new celda[n][m];
 		for(int i = 0; i<n; i++) {
 			for(int j = 0; j<m; j++) {
@@ -55,20 +45,7 @@ public class rompecabezas {
 		}
 	}
 
-	public static void leerCoordenadasPosiciones(Scanner scan, boolean esObjetivo) {
-		for(int i = 0; i<numeroBolas; i++) {
-			int x = scan.nextInt();
-			int y = scan.nextInt();
-			if(esObjetivo) {
-				tablero[x-1][y-1] = new celda(true,null);
-			}else{
-				tablero[x-1][y-1].setBola(i+1);
-			}
-			
-		}
-	}
-
-	private static void imprimirTablero() {
+	public void imprimir() {
 		String s = "";
 
 		for(int i = 0; i<n; i++) {
@@ -80,4 +57,40 @@ public class rompecabezas {
 			System.out.println();
 		}
 	}
+
+	public void leerCoordenadasPosiciones(Scanner scan, int numeroBolas, boolean esObjetivo) {
+		for(int i = 0; i<numeroBolas; i++) {
+			int x = scan.nextInt();
+			int y = scan.nextInt();
+			if(esObjetivo) {
+				tablero[x-1][y-1] = new celda(true,null);
+			}else{
+				tablero[x-1][y-1].setBola(i+1);
+			}
+			
+		}
+	}
+}
+
+public class rompecabezas {
+	private static int n = 0;
+	private static int m = 0;
+	private static int numeroBolas = 0;
+	private static Tablero  tablero = null;
+
+	public static void main(String[] arg) {
+
+		Scanner scan = new Scanner(System.in);
+
+		n = scan.nextInt();
+		m = scan.nextInt();
+		tablero = new Tablero(n,m);
+		
+		numeroBolas = scan.nextInt();
+
+		tablero.leerCoordenadasPosiciones(scan, numeroBolas, true);
+		tablero.leerCoordenadasPosiciones(scan, numeroBolas, false);
+		tablero.imprimir();
+	}
+
 }
